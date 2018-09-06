2bda15110b1c98f1967d48442ef239411bcb5152
Domantas Grotuzas <DomantasGrotuzas@gmail.com>
2018-09-06 11:57:39 +0300

    Create channel to send and receive data

---
+++ TiesinisKodas/Code.Contracts/IChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Contracts
+{
+    public interface IChannel
+    {
+        void Send(BitArray data, int failureRate);
+        BitArray Result { get; }
+    }
+}
---
+++ TiesinisKodas/Code.Implementation/Channel.cs
@@ -0,0 +1,26 @@
+﻿using Code.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Implementation
+{
+    public class Channel : IChannel
+    {
+        public BitArray Result { get; private set; }
+
+        public void Send(BitArray data, int failureRate)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < data.Count; i++)
+                if (rand.Next(1, 101) <= failureRate)
+                    data[i] = !data[i];
+
+            Result = data;
+        }
+    }
+}
